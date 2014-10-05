c6513d20e30e716cfc7ec15d3cff0bc9ac7e8384
Vaibhav Pandey <pandey.vaibhav@gmail.com>
2014-10-05 08:18:01 +0530

    Learning Swift. Initial Commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLable: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +31,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        // code will evaluate when we press the button.
+        messageLable.hidden = false;
+        messageLable.text = enterAMessageTextField.text;
+        messageLable.textColor = UIColor.redColor();
+        
+        
+        enterAMessageTextField.text = " ";
+        enterAMessageTextField.resignFirstResponder();
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal);
+        
+        // unhide the name label. (challenge 1)
+        nameLabel.hidden = false ;
+        nameLabel.text = enterNameTextField.text ;
+        nameLabel.textColor = UIColor.blueColor() ;
+        enterNameTextField.text = " ";
+        enterNameTextField.resignFirstResponder();
+        
+        
+
+        
+        
+        
+    }
 }
 
